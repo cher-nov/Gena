44c5b5bef167b5d63fcfe1b098cdb98874941718
Dmitry D. Chernov <blackdoomer@yandex.ru>
2017-03-11 04:59:06 +1000

    [Tests] Generalized typesets. This wouldn't have been possible without 103393ab2dabfba10848c423b25e4811e7b043ab.

--- tests/gvec_general.h
+++ tests/gvec_general.h
@@ -3,4 +3,13 @@
 
 #include "../genvector.h"
 
+#define __GVT_TYPESET_TO_ARGS(template, typeset) \
+  template typeset
+
+#define __GVTEST_TYPESET_INTV \
+  (int, intv, GVEC_USE_VAL, GVEC_USE_VAL)
+
+#define __GVTEST_TYPESET_INTR \
+  (int, intr, GVEC_USE_REF, GVEC_USE_REF)
+
 #endif /*__GVEC_GENERAL_H__*/
